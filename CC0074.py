474417082d279f0908ac712094d1d35652efc581
mengo-eng <melnur@gmail.com>
2021-01-19 23:54:46 +0000

    added CC0074.py

---
+++ CC0074.py
@@ -0,0 +1,9 @@
+divs = []
+n = int(input("Enter a number: "))
+for i in range(2, n+1):
+    if n % i == 0:
+        divs.append(i)
+if len(divs) < 2 and n != 1:
+    print(f"{n} is a prime number")
+else:
+    print(f"{n} is not a prime number")
